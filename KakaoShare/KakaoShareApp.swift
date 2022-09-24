1714333d5c8ac8a4f68488b3d21972cb5160da55
skyepodium <skyepodium@gmail.com>
2022-09-24 15:41:36 +0900

    Initial Commit

---
+++ KakaoShare/KakaoShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoShareApp.swift
+//  KakaoShare
+//
+//  Created by 김정윤 on 2022/09/24.
+//
+
+import SwiftUI
+
+@main
+struct KakaoShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
